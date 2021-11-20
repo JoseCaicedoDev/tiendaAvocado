6850a99efcf8806afa6f27a3bc49bbed1681f825
jcaicedo <jose.caicedo@outlook.com>
2021-11-20 16:56:51 -0500

    deploy DOM

--- src/index.js
+++ src/index.js
@@ -2,5 +2,43 @@
  * This file is just a silly example to show everything working in the browser.
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
+const url = 'https://platzi-avo.vercel.app/api/avo';
 
-console.log('Happy hacking :)')
+const appNode = document.querySelector('#app');
+
+const formatPrice = (price) =>{
+  const newPrice = new window.Intl.NumberFormat('en-EN',{
+    style: 'currency',
+    currency: 'USD'
+  }).format(price)
+ return newPrice;
+}
+//web api
+//conectarnos al server
+window
+  .fetch(url)
+//procesar la respuesta, y convertirla en JSON
+  .then((respuesta) => respuesta.json())
+//JSON -> Data -> renderizar info browser
+  .then((responseJson) =>{
+    const allItems = []
+/*     let i =0; */
+    responseJson.data.forEach((item)=>{
+      const imagen = document.createElement('img');
+      imagen.src = `https://platzi-avo.vercel.app${item.image}`;
+      imagen.alt = item.name;
+      const title = document.createElement('h2');
+      title.textContent = item.name;
+      title.className = 'text-2xl font-bold text-gray-900';
+      const price = document.createElement('p');
+      price.textContent = formatPrice(item.price);
+      price.className = 'text-lg text-red-500';
+
+      const container = document.createElement('div');
+      container.append(imagen, title, price);
+      allItems.push(container);
+      container.className = 'max-w-sm bg-white shadow-xl rounded-lg p-5';
+/*       console.log(`${i++}.- ${item.name}`) */
+    });
+    appNode.append(...allItems);
+  });
