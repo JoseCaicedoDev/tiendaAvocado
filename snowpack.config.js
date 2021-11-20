658c7c76ff3ea93e065bba058d242e49cf2d595f
jcaicedo <jose.caicedo@outlook.com>
2021-11-20 17:58:14 -0500

    enlaces1

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: 'https://github.com/JoseCaicedoDev/tiendaAvocado/actionst',
+    baseUrl: 'https://github.com/JoseCaicedoDev/tiendaAvocado/actions',
   },
 }
