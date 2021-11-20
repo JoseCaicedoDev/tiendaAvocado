4416d163a1d00a3922b2e36d843e73f6099cd599
jcaicedo <jose.caicedo@outlook.com>
2021-11-20 18:07:58 -0500

    enlaces3

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,6 +5,6 @@ module.exports = {
     src: '/_dist_',
   },
   buildOptions: {
-    baseUrl: 'https://github.com/JoseCaicedoDev/tiendaAvocado/gh-pages',
+    baseUrl: 'https://raw.githubusercontent.com/JoseCaicedoDev/tiendaAvocado/gh-pages',
   },
 }
